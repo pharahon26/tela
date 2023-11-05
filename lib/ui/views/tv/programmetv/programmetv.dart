4c7fc447efa5ca58975f2dd97e4e5229fce59f8f
pharahon26 <seremehabibf@gmail.com>
2023-11-05 11:50:15 +0000

    modif programme tv

--- lib/ui/views/tv/programmetv/programmetv.dart
+++ lib/ui/views/tv/programmetv/programmetv.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mobile/ui/views/tv/programmetv/programmetvViewModel.dart';
+import 'package:mobile/ui/widget/tela_t_v_programe_card.dart';
 import 'package:stacked/stacked.dart';
 
 class ProgrammeTv extends StatefulWidget {
@@ -16,8 +17,83 @@ class _ProgrammeTvState extends State<ProgrammeTv> {
     return ViewModelBuilder<ProgrammeTvViewModel>.reactive(
       viewModelBuilder: () => ProgrammeTvViewModel(),
       builder: (context, model, child) => Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-          body: Center(child:  Text('ProgrammeTv Tela'))
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            centerTitle: true,
+            title: const Text('Tela TV',
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.3,
+                  color: Colors.white
+              ),
+            ),
+            elevation: 5,
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(Icons.menu),
+                  onPressed: () { Scaffold.of(context).openDrawer(); },
+                  tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                );
+              },
+            ),
+          ),
+          drawer: Drawer(
+            elevation: 5,
+            child: SafeArea(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    DrawerHeader(child: Center(
+                      child: Image.asset('assets/images/logo.png'),
+                    )),
+                    TextButton(
+                        onPressed: (){
+                          model.navigateToAcceuil();
+                        },
+                        child: Text('Acceuil')
+                    ),
+                    TextButton(
+                        onPressed: (){
+                          model.navigateToProfile();
+                        },
+                        child: Text('profile')
+                    ),
+                    TextButton(
+                        onPressed: (){
+                          model.navigateToRechercheLogement();
+                        },
+                        child: Text('Trouver un logement')
+                    ),
+                    TextButton(
+                        onPressed: (){
+                          model.navigateToRechercheBureau();
+                        },
+                        child: Text('Trouver un Bureau')
+                    ),
+                    TextButton(
+                        onPressed: (){
+                          model.navigateToTV();
+                        },
+                        child: Text('Tela TV')
+                    ),
+                    TextButton(
+                        onPressed: (){
+                          model.navigateToEbank();
+                        },
+                        child: Text('Tela Banking')
+                    ),
+                  ]),
+            ),
+          ),
+          body: ListView(
+            children: model.program.map((e) => InkWell(
+              onTap: () => model.navigateToLiveTV(e),
+                child: TelaTVProgrameCard(programmeTV: e,)
+            )).toList(),
+          )
       ),
     );
   }
--- lib/ui/views/tv/programmetv/programmetvViewModel.dart
+++ lib/ui/views/tv/programmetv/programmetvViewModel.dart
@@ -1,19 +1,46 @@
 import 'package:mobile/app/app.locator.dart';
 import 'package:mobile/app/app.router.dart';
+import 'package:mobile/models/programetv.dart';
+import 'package:mobile/services/tv_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ProgrammeTvViewModel extends BaseViewModel{
 
   NavigationService _navigationService = locator<NavigationService>();
+  TVService _tvService = locator<TVService>();
   DialogService _dialogService = locator<DialogService>();
   SnackbarService _snackbarService = locator<SnackbarService>();
 
+  List<TelaProgrammeTV> program = [];
+  ProgrammeTvViewModel(){
+    program = _tvService.progTest();
+  }
 
-  ProgrammeTvViewModel();
-
+  void navigateToEbank() async{
+    await _navigationService.navigateTo(Routes.bank);
+  }
   void navigateToProfile() async{
+    await _navigationService.navigateTo(Routes.profile);
+  }
+  void navigateToTV() async{
+    await _navigationService.navigateTo(Routes.programmeTv);
+  }
+
+  void navigateToRechercheBureau() async{
+    await _navigationService.navigateToRecherche(isBureau: true);
+  }
+  void navigateToRechercheLogement() async{
+    await _navigationService.navigateToRecherche(isBureau: false);
+  }
+  void navigateToGalery() async{
+    await _navigationService.navigateTo(Routes.galerie);
+  }
+  void navigateToAcceuil() async{
     await _navigationService.navigateTo(Routes.acceuil);
+  }
+  void navigateToLiveTV(TelaProgrammeTV programmeTV) async{
+    await _navigationService.navigateToTv(programmeTV: programmeTV);
   }
 
 
